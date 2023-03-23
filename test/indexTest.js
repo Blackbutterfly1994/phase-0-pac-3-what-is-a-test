06b702084e3c3e764bb29801356a7c41ccd94a6c
Lashanique <lcars34@wgu.edu>
2023-03-23 14:29:38 -0400

    complete lab

--- test/indexTest.js
+++ test/indexTest.js
@@ -2,14 +2,12 @@ require ( './root.js' );
 
 const { name, height, message } = require("../index.js");
 
-/*
 describe('what-is-a-test', () => {
   describe('Name', () => {
     it('returns "Susan"', () => {
       expect(name).toEqual('Susan')
     })
   })
-
 
   describe('Height', () => {
     it('is less than 40', () => {
@@ -24,7 +22,7 @@ describe('what-is-a-test', () => {
     })
   })
 })
-*/
+
 
 // The code below ensures that students who are using CodeGrade will get credit 
 // for the code-along in Canvas; you can disregard it.
